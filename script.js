50526f0e7b5d4ca8bdbfe2c54623da21374ba22c
yohann-devweb3 <yohann.huedro@gmail.com>
2022-10-01 00:19:09 +1100

    update css

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-
+// set les progress bar de facon animée
 function setProgressBars(){
-    array=["pb_HTML","pb_JS","pb_MICRO","pb_SQL","pb_PYTHON"];
+    array=["pb_HTML","pb_JS","pb_MICRO","pb_SQL","pb_PYTHON"]; // charge les ID des progressBar dans un tableau
     for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        console.log(element);
+        const element = array[index]; 
 
-    
-   
         var width = 20;
         var id = setInterval(frame, 62);
         function frame() {
@@ -15,7 +12,7 @@ function setProgressBars(){
           } else {
             var elem = document.getElementById(element);  
             width++; 
-            elem.style.width = width + '%'; 
+            elem.style.width = width + '%';  // change pourcentages
             elem.innerHTML = width * 1  + '%';
           }
         }
