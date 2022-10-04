7dfa6ae0b0460541d60548dd4ef4892fc5a71509
yohann-devweb3 <yohann.huedro@gmail.com>
2022-10-04 20:41:10 +1100

    update css : rajout du about cyan

--- script.js
+++ script.js
@@ -19,3 +19,57 @@ function setProgressBars(){
     }
   }
 
+  // 'rounded' is the class I'm applying to the toast
+  M.toast({html: 'Bienvenue sur mon cv en ligne !', classes: 'rounded'});
+
+
+
+
+/***************************execute quand page est chargée*************** */
+    $( document ).ready(function() {
+      // Handler for .ready() called.
+
+      //initialisation 
+      $(".tap-target").tapTarget();
+      $("#menu").hide();
+
+             
+      $( window ).resize(function() {
+        var maTaille = $( window ).width(); // recupere la width de la fenetre
+        maTaille = parseInt(maTaille);
+        console.log(maTaille);
+        if (maTaille>1247){
+          $("#menu").show();
+          $(".tap-target").tapTarget('open');
+    
+          $("#btnRedirect").addClass( "pulse" );
+        } else{
+          $("#menu").hide();
+          $(".tap-target").tapTarget('close');
+          $("#btnRedirect").removeClass( "pulse" );
+        }
+      });
+
+     
+     $(window).scroll(function (event) { // trigg quand on scroll
+      var mywidth = $( window ).width(); // recupere la width de la fenetre
+      var scroll = $(window).scrollTop(); //recupere la ligne ou se scroll
+     
+      
+      parseInt(scroll);
+      parseInt(mywidth);
+      
+
+        if ((scroll>500)&&(mywidth>949)){
+          $("#menu").show();
+          $(".tap-target").tapTarget('open');
+    
+          $("#btnRedirect").addClass( "pulse" );
+        } else{
+          $("#menu").hide();
+          $(".tap-target").tapTarget('close');
+          $("#btnRedirect").removeClass( "pulse" );
+        }
+       
+    });
+  });
